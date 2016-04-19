93dc19fe3162b159a7aba4b0acadfaab6a8e750c
romarpla <jrodrigo.martinez@hotmail.com>
2016-04-18 19:39:11 -0500

    exception

---
+++ proyectoATM/src/OverdraftException.java
@@ -0,0 +1,14 @@
+public class OverdraftException extends Exception{
+	private double deficit;
+	
+	public OverdraftException(String msj, double deficit){
+		super(msj);
+		this.deficit = deficit;
+	}
+	
+	public double getDeficit(){
+		return this.deficit;
+	}		
+	
+}
+

77a1ad655cf94016da368a6619aa03813527d007
romarpla <jrodrigo.martinez@hotmail.com>
2016-04-25 18:39:25 -0500

    solo faltan cambio de nip y consulta de mov

--- proyectoATM/src/TestAtmGraphics.java
+++ proyectoATM/src/TestAtmGraphics.java
@@ -2,6 +2,8 @@ import banco.*;
 import atm.*;
 import cuentas.*;
 import frames.*;
+import personales.*;
+
 public class TestAtmGraphics{
 	public static void main(String args[]){
 		AtmGraphics atm = new AtmGraphics();
